18d65e0b70109e214d817c4950f372bcc62ee74d
nandinicbit1981 <nandinicbit@gmail.com>
2017-03-05 15:39:49 -0800

    Fixing minor issues.

--- app/src/main/java/parimi/com/movieapp/HttpUtils.java
+++ app/src/main/java/parimi/com/movieapp/HttpUtils.java
@@ -14,18 +14,6 @@ public class HttpUtils {
         client.get(getAbsoluteUrl(relativeUrl), params, responseHandler);
     }
 
-    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.post(getAbsoluteUrl(url), params, responseHandler);
-    }
-
-    public static void getByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.get(url, params, responseHandler);
-    }
-
-    public static void postByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.post(url, params, responseHandler);
-    }
-
     private static String getAbsoluteUrl(String relativeUrl) {
         return BASE_URL + relativeUrl;
     }
