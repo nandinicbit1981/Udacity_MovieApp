7aab961642d87e25468bb52feaa3a496416f48ce
nandinicbit1981 <nandinicbit@gmail.com>
2017-03-07 12:26:58 -0800

    everything works

--- app/src/main/java/parimi/com/movieapp/MainActivityFragment.java
+++ app/src/main/java/parimi/com/movieapp/MainActivityFragment.java
@@ -1,14 +1,17 @@
 package parimi.com.movieapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.GridView;
 
 import com.google.gson.Gson;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -31,24 +34,24 @@ public class MainActivityFragment extends Fragment {
 
     private MovieAdapter movieAdapter;
     private View rootView;
-    private GridView gridView;
     private static String STATE_POSITION = "STATE_POSITION";
     private static String MOVIE_LIST = "MOVIE_LIST";
     int mCurrentPosition = 0;
     private ArrayList<Movie> results = null;
     private String sortOrder = "";
-
+    private RecyclerView recyclerView;
+    private static final String BUNDLE_RECYCLER_LAYOUT = "classname.recycler.layout";
     public MainActivityFragment() {
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        if(gridView != null) {
-            mCurrentPosition = gridView.getFirstVisiblePosition();
+
+            savedInstanceState.putParcelable(BUNDLE_RECYCLER_LAYOUT, recyclerView.getLayoutManager().onSaveInstanceState());
             savedInstanceState.putInt(STATE_POSITION, mCurrentPosition);
             savedInstanceState.putParcelableArrayList(MOVIE_LIST, results);
-        }
+
 
     }
 
@@ -96,27 +99,12 @@ public class MainActivityFragment extends Fragment {
                 try {
                     results = createMovieList(response.getJSONArray("results"));
                     if(movieAdapter ==null){
-                        movieAdapter = new MovieAdapter(getActivity(), results);
+                        movieAdapter = createAdapter(results);
                     }
-                    gridView = ((GridView) rootView);
-                    gridView.setAdapter(movieAdapter);
+                    recyclerView = ((RecyclerView) rootView.findViewById(R.id.my_recycler_view));
+                    recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), Utility.calculateNoOfColumns(getActivity())));
+                    recyclerView.setAdapter(movieAdapter);
                     movieAdapter.notifyDataSetChanged();
-                    gridView.smoothScrollToPosition(mCurrentPosition);
-                    gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-
-                            Intent intent = new Intent(getActivity(), DetailActivity.class);
-                            try {
-                                Gson gson = new Gson();
-                                String movieJson = gson.toJson(results.get(position));
-                                intent.putExtra("movieInfo", movieJson);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            startActivity(intent);
-                        }
-                    });
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -138,8 +126,14 @@ public class MainActivityFragment extends Fragment {
         super.onViewStateRestored(savedInstanceState);
         // Restore UI state from the savedInstanceState.
         if(savedInstanceState != null) {
+            recyclerView = ((RecyclerView) rootView.findViewById(R.id.my_recycler_view));
             mCurrentPosition = savedInstanceState.getInt(STATE_POSITION);
             results = savedInstanceState.<Movie>getParcelableArrayList(MOVIE_LIST);
+            Parcelable savedRecyclerLayoutState = savedInstanceState.getParcelable(BUNDLE_RECYCLER_LAYOUT);
+            movieAdapter = createAdapter(results);
+            recyclerView.setAdapter(movieAdapter);
+            recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), Utility.calculateNoOfColumns(getActivity())));
+            recyclerView.getLayoutManager().onRestoreInstanceState(savedRecyclerLayoutState);
         }
     }
 
@@ -147,20 +141,50 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        recyclerView = ((RecyclerView) rootView.findViewById(R.id.my_recycler_view));
         if(savedInstanceState != null && savedInstanceState.getParcelableArrayList(MOVIE_LIST) != null) {
-            movieAdapter = new MovieAdapter(getActivity(), savedInstanceState.<Movie>getParcelableArrayList(MOVIE_LIST));
-            gridView = ((GridView) rootView);
-            gridView.setAdapter(movieAdapter);
-            gridView.smoothScrollToPosition(savedInstanceState.getInt(STATE_POSITION));
+            movieAdapter = createAdapter(savedInstanceState.<Movie>getParcelableArrayList(MOVIE_LIST));
+            recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), Utility.calculateNoOfColumns(getActivity())));
+            recyclerView.setAdapter(movieAdapter);
+            movieAdapter.notifyDataSetChanged();
         } else {
             getMovieList();
         }
         return rootView;
     }
 
+
     private String getSortPreference() {
         SharedPreferences sharedPreferences = getActivity().getApplicationContext().getSharedPreferences("MoviePrefs", MODE_PRIVATE);
         return sharedPreferences.getString("sortOptions","popular");
+    }
+
+    private MovieAdapter createAdapter(ArrayList<Movie> results) {
+        return new MovieAdapter(results, getActivity().getApplicationContext(),new MovieAdapter.OnItemClickListener() {
+            @Override public void onItemClick(Movie item) {
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                try {
+                    Gson gson = new Gson();
+                    String movieJson = gson.toJson(item);
+                    intent.putExtra("movieInfo", movieJson);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                getActivity().startActivity(intent);
+            }
+        });
+    }
+
+}
+
+
+class Utility {
+    public static int calculateNoOfColumns(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        int noOfColumns = (int) (dpWidth / 180);
+        return noOfColumns;
     }
 }
--- app/src/main/java/parimi/com/movieapp/MovieAdapter.java
+++ app/src/main/java/parimi/com/movieapp/MovieAdapter.java
@@ -1,86 +1,92 @@
 package parimi.com.movieapp;
 
-import android.app.Activity;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
-import java.util.List;
+import java.util.ArrayList;
 
-public class MovieAdapter extends ArrayAdapter<Movie> {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
+    private ArrayList<Movie> mDataset;
+    private Context context;
+    private final OnItemClickListener listener;
 
-    /**
-     * This is our own custom constructor (it doesn't mirror a superclass constructor).
-     * The context is used to inflate the layout file, and the List is the data we want
-     * to populate into the lists
-     *
-     * @param context        The current context. Used to inflate the layout file.
-     * @param movies A List of movies objects to display in a list
-     */
-    public MovieAdapter(Activity context, List<Movie> movies) {
-        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
-        // the second argument is used when the ArrayAdapter is populating a single TextView.
-        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
-        // going to use this second argument, so it can be any value. Here, we used 0.
-        super(context, 0, movies);
-    }
-
-
-    /**
-     * Provides a view for an AdapterView (ListView, GridView, etc.)
-     *
-     * @param position    The AdapterView position that is requesting a view
-     * @param convertView The recycled view to populate.
-     *                    (search online for "android view recycling" to learn more)
-     * @param parent The parent ViewGroup that is used for inflation.
-     * @return The View for the position in the AdapterView.
-     */
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        // Gets the MovieAdapter object from the ArrayAdapter at the appropriate position
-        Movie movie = getItem(position);
-
-        // Adapters recycle views to AdapterViews.
-        // If this is a new View object we're getting, then inflate the layout.
-        // If not, this view already has the layout inflated from a previous call to getView,
-        // and we modify the View widgets as usual.
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.flavor_item, parent, false);
-        }
-
-        ImageView iconView = (ImageView) convertView.findViewById(R.id.flavor_image);
-
-        String url = movie.poster_path;
-
-        Glide
-                .with(getContext())
-                .load("http://image.tmdb.org/t/p/w185/" + url)
-                .centerCrop()
-                .crossFade()
-                .into(iconView);
-
-
-
-        return convertView;
+    public interface OnItemClickListener {
+        void onItemClick(Movie item);
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
         public ImageView iconView;
-        public CheckBox item_check;
 
-        public ViewHolder(View convertView) {
-            super(convertView);
-
-            iconView  = (ImageView) convertView.findViewById(R.id.flavor_image);
+        public ViewHolder(View v) {
+            super(v);
+            iconView = (ImageView) v.findViewById(R.id.flavor_image);
         }
+
+        public void bind(final Movie item, final OnItemClickListener listener) {
+            String url = item.poster_path;
+            Glide
+                    .with(context)
+                    .load("http://image.tmdb.org/t/p/w185/" + url)
+                    .centerCrop()
+                    .crossFade()
+                    .into(iconView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    listener.onItemClick(item);
+                }
+            });
+        }
+
     }
+
+    public void add(int position, Movie item) {
+        mDataset.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void remove(Movie item) {
+        int position = mDataset.indexOf(item);
+        mDataset.remove(position);
+        notifyItemRemoved(position);
+    }
+
+
+    public MovieAdapter(ArrayList<Movie> movies, Context context, OnItemClickListener listener) {
+        mDataset = movies;
+        this.context = context;
+        this.listener = listener;
+    }
+
+    @Override
+    public MovieAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.flavor_item, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.bind(mDataset.get(position), listener);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
 }
